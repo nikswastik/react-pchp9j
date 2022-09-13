d64feab610f5f56b1739bfcbbfbc5f0c0399c351
nikswastik <48277777+nikswastik@users.noreply.github.com>
2022-09-13 08:09:19 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-pchp9j
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-pchp9j)
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+import { Login } from './Login';
+export default function App() {
+  return (
+    <div>
+      <Login />
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
